f9af5682fddf75488cb66067da7c2705096fc25e
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-01-29 17:44:51 +0900

    feat: PagingView & FloatingAction 생성 및 오픈 소스 pod install 완료

---
+++ PagingView_Tutorial/PagingView_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PagingView_Tutorial
+//
+//  Created by 목정아 on 2023/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IOS_OpenSource_Study
+
+openSource 스터디한 프로젝트 올리기
